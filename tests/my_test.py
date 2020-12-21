cd444d376e0b777462eea12ba098d1041cd94421
JackCowling <jack@cowling.org.uk>
2020-12-21 21:48:46 +0000

    Commented failing test

--- tests/my_test.py
+++ tests/my_test.py
@@ -4,5 +4,5 @@ from your_package_here.import_test import test_function_import
 def test_which_passes():
     assert test_function_import() == "Function successfully imported"
 
-def test_which_fails():
-    assert test_function_import() == "This is not the correct output"
+# def test_which_fails():
+#     assert test_function_import() == "This is not the correct output"
