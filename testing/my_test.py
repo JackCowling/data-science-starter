f0308fdaadda0ade718af70162d84f78c432f143
Jack Cowling <jack.cowling@mesh-ai.com>
2022-10-27 11:55:07 +0100

    Updated to use reqs file

--- testing/my_test.py
+++ testing/my_test.py
@@ -1,10 +1,10 @@
 import pytest
-from your_package_here.import_test import test_function_import
+from your_package_here.example import function_import
 
 
 def test_which_passes():
-    assert test_function_import() == "Function successfully imported"
+    assert function_import() == "Function successfully imported"
 
 
 def test_which_fails():
-    assert test_function_import() == "This is not the correct output"
+    assert function_import() == "This is not the correct output"
