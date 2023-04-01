4c7038f7bba2b4f9adf8665ff41b6369d7da2e47
Jack Cowling <jack@cowling.org.uk>
2023-04-01 11:01:24 +0100

    Get checks working

--- src/your_package_here/example.py
+++ src/your_package_here/example.py
@@ -1,5 +1,2 @@
 def function_import() -> str:
     return "Function successfully imported"
-
-def function_not_typed_or_tested():
-    pass
--- testing/my_test.py
+++ testing/my_test.py
@@ -1,10 +1,5 @@
-import pytest
 from your_package_here.example import function_import
 
 
 def test_which_passes():
     assert function_import() == "Function successfully imported"
-
-
-def test_which_fails():
-    assert function_import() == "This is not the correct output"
